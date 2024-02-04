4a7520150d94463d9d5325e8afbc2e4826dac128
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-02-04 23:59:52 +0800

    Update print_bits.c

--- level1/print_bits.c
+++ level1/print_bits.c
@@ -15,21 +15,31 @@ Example, if you pass 2 to print_bits, it will print "00000010"
 */
 
 #include <unistd.h>
-void	print_bit(unsigned char octet)
+void	print_bits(unsigned char octet)
 {
-	int i = 7;
-	while(i >= 0)
+	int bit = 8;
+	while (0 <= bit)
 	{
-		if (octet & (1 << i)) // "<<" it move the right 0 to left, "&" is to check whether is the same
+		if (octet & (1 << bit)) // "<<" check start from right it is 1. "&" is to compare the both bit
 			write(1, "1", 1);
 		else
 			write(1, "0", 1);
-		i--;
+		bit--;
 	}
-	write(1, "\n", 1);
 }
 
 int main()
 {
-	print_bit(24);
+	print_bit(2);
 }
+/*
+*example: 0000 0010 & 1000 0000 = 0
+	  0000 0010 & 0100 0000 = 0
+   	  0000 0010 & 0010 0000 = 0
+   	  0000 0010 & 0001 0000 = 0
+          0000 0010 & 0000 1000 = 0
+	  0000 0010 & 0000 0100 = 0
+	  0000 0010 & 0000 0010 = 1
+	  0000 0010 & 0000 0001 = 0
+	  result will be 0000 0010
+*/
