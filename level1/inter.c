ac1490764b6af5dc3298a576bc8190915fc64c97
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-03-10 15:53:19 +0800

    Update inter.c

--- level1/inter.c
+++ level1/inter.c
@@ -61,3 +61,29 @@ int main(int ac, char **av)
 	}
 	write(1, "\n", 1);
 }
+
+//ver2
+int main(int ac, char **av)
+{
+        if (ac == 3)
+        {
+                int i = 0;
+                int tab[127] = {0};
+                while (av[2][i])
+                {
+                        tab[(int)av[2][i]] = 1;
+                        i++;
+                }
+                i = 0;
+                while (av[1][i])
+                {
+                        if (tab[(int)av[1][i]] == 1)
+                        {
+                                write(1, &av[1][i], 1);
+                                tab[(int)av[1][i]] = 2;
+                        }
+                        i++;
+                }
+        }
+        write(1, "\n", 1);
+}
