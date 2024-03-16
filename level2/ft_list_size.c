c8e674c7ee7efee923f4f46e538dc12427b9ee7b
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-03-16 15:32:57 +0800

    Update ft_list_size.c

--- level2/ft_list_size.c
+++ level2/ft_list_size.c
@@ -9,9 +9,10 @@ passed to it.
 
 It must be declared as follows:
 
-int	ft_list_size(t_list *begin_list);
+int ft_list_size(t_list *begin_list);
 
-You must use the following structure in your program ft_list_size.c :
+You must use the following structure, and turn it in as a file called
+ft_list.h:
 
 typedef struct    s_list
 {
@@ -19,19 +20,45 @@ typedef struct    s_list
     void          *data;
 }                 t_list;
 */
+//ft_list.h
+// #ifndef FT_LIST_H
+// # define FT_LIST_H
 
+// typedef struct s_list
+// {
+// struct s_list *next;
+// void *data;
+// } t_list;
 
-typedef struct    s_list
+// #endif
+
+#include "ft_list.h"
+int ft_list_size(t_list *begin_list)
 {
-    struct s_list *next;
-    void          *data;
-}                 t_list;
+	int i = 0;
+	while (begin_list)
+	{
+		begin_list = begin_list->next;
+		i++;
+	}
+	return i;
+}
 
-int	ft_list_size(t_list *begin_list)
+
+#include <stdio.h>
+int main()
 {
-	if (begin_list == 0)
-		return 0;
-	else
-		return (1 + ft_list_size(begin_list->next));
+	t_list a, b, c;
+	a.next = &b;
+	b.next = &c;
+	c.next = NULL;
+
+	int data1 = 1, data2 = 2, data3 = 3;
+	a.data = &data1;
+	b.data = &data2;
+	c.data = &data3;
+
+	t_list *ptr = &a;
+	printf("%d\n", ft_list_size(ptr));
 }
 
