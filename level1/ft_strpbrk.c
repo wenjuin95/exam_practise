e94df5f96e32f13a6b3cb7838049b8291e6b1c27
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-03-24 20:03:24 +0800

    Update ft_strpbrk.c

--- level1/ft_strpbrk.c
+++ level1/ft_strpbrk.c
@@ -11,19 +11,29 @@
 // char	*ft_strpbrk(const char *s1, const char *s2);
 
 //similar to ft_strchr
+
+/*
+*	0. check if empty
+*	1. loop the pointer of s1 (not string)
+*	2. loop the s2 string and compare with *s1
+*	3. if found return the pointer position s1(typecast to char *)
+*	4. if not return 0;
+*/
 char *ft_strpbrk(const char *s1, const char *s2)
 {
-	int i = 0;
-	while (*s1)
+	if (s1 == 0 || s2 == 0)
+		return 0;
+	int j = 0;
+	while (s1[j])
 	{
 		i = 0;
 		while (s2[i])
 		{
-			if (*s1 == s2[i])
+			if (s1[j] == s2[i])
 				return (char *)s1;
 			i++;
 		}
-		s1++;
+		j++;
 	}
 	return 0;
 }
