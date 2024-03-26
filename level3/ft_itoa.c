86ccee3b0143064d9fbcc4d33a4864a910f721fc
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-03-26 12:02:45 +0800

    Update ft_itoa.c

--- level3/ft_itoa.c
+++ level3/ft_itoa.c
@@ -12,34 +12,53 @@ Your function must be declared as follows:
 char	*ft_itoa(int nbr);
 */
 
-int nbr_len(int nbr)
+
+/*
+*	1. get the length of the number
+*	2. if found negative number increase the number length
+*	3. malloc the number length
+*	4. put the null terminator at the back
+*	5. check if is 0 then store 0 and return
+*	6. if is negative number store "-" and make it positive
+*	7. make a loop to store the number from back
+*	8. return the string
+*/
+int nbr_len(int n) //get the number length
 {
-	int i = 1;
-	while (nbr /= 10)
+	int i = 0;
+	while (n)
+	{
+		n /= 10;
 		i++;
+	}
 	return i;
 }
 
 char *ft_itoa(int nbr)
 {
-	char *result;
-	int len;
-
-	long num = nbr;
-	len = nbr_len(num);
-	if (num < 0)
-	{
-		num *= -1;
+	long n = nbr;
+	int len = nbr_len(n);
+	if (n <= 0) //check if is negative increase the length
 		len++;
-	}
-	result = malloc(sizeof(char) * (len + 1));
-	result[len] = '\0';
-	while (len--)
+	char *res = malloc(sizeof(char) * len + 1);
+	if (res == NULL)
+		return NULL;
+	res[len - 1] = '\0'; //it put null terminator to the end of the length
+	if (n == 0) //if number is 0 just assign 0 and return 0
 	{
-		result[len] = num % 10 + '0';
-		num = num / 10;
+		res[0] = '0';
+		return res;
 	}
-	if (nbr < 0) //use ori nbr to know is "-" number
-		result[0] = '-';
-	return (result);
+	if (n < 0) //if is negative assign "-" and make it positive number
+	{
+		res[0] = '-';
+		n *= -1;
+	}
+	while (n) //loop the n
+	{
+		len--;
+		res[len] = n % 10 + '0'; // this get the end one by one and store
+		n /= 10; //this will delete end one by one
+	}
+	return res;
 }
