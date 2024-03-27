c5704359d054d7fc86cbefb6058da63f60792798
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-03-27 11:31:53 +0800

    Update str_capitalizer.c

--- level2/str_capitalizer.c
+++ level2/str_capitalizer.c
@@ -30,49 +30,6 @@ $>
 
 #include <unistd.h>
 
-void str_cap(char *str)
-{
-	while(*str)
-	{
-		while(*str && (*str == ' ' || *str == '\t'))
-		{
-			write(1, str, 1);
-			str++;
-		}
-		if (*str != '\0')
-		{
-			if (*str >= 'a' && *str <= 'z')
-				*str = *str - 32;
-			write(1, str, 1);
-			str++;
-		}
-		while(*str && *str != ' ' && *str != '\t')
-		{
-			if(*str >= 'A' && *str <= 'Z')
-				*str = *str + 32;
-			write(1, str, 1);
-			str++;
-		}
-	}
-	write(1, "\n", 1);
-}
-
-int main(int ac, char **av)
-{
-	if (ac == 1)
-		write(1, "\n", 1);
-	else
-	{
-		int i = 1;
-		while (i < ac)
-		{
-			str_cap(av[i]);
-			i++;
-		}
-	}
-}
-
-//ver 2
 /*
 *	1. loop through the whole argument
 *	2. loop through each of the word
@@ -92,29 +49,28 @@ int main(int ac, char **av)
 		{
 			//loop through the word
 			int j = 0;
-			char c; //for the word chg
 			int flag = 0; //make flag for found the first word
 			while (av[i][j])
 			{
 				//check is 'A' to 'Z'
 				if (av[i][j] >= 'A' && av[i][j] <= 'Z')
-					c = av[i][j] + 32; //chg to 'a' to 'z'
+					av[i][j] = av[i][j] + 32; //chg to 'a' to 'z'
 				else
-					c = av[i][j]; //maintain
+					av[i][j] = av[i][j]; //maintain
 				//when next word is space
-				if (c == ' ' || av[i][j] == '\t')
+				if (av[i][j] == ' ' || av[i][j] == '\t')
 						flag = 1; //open flag
 				//check the flag is true and the next word is 'a' to 'z' 
-				if (c >= 'a' && c <= 'z' && (flag == 1 || j == 0))
+				if (av[i][j] >= 'a' && av[i][j] <= 'z' && (flag == 1 || j == 0))
 				{
-					c = c - 32; //chg to 'A' to 'Z'
+					av[i][j] = av[i][j] - 32; //chg to 'A' to 'Z'
 					flag = 0; //close flag
 				}	
 				write(1, &c, 1); //write out
 				j++;
 			}
-			i++;
 			write(1,"\n", 1); //if have more argument make it line by line
+			i++;
 		}
 	}
 	else
