1806a2d41d53543790ac2e0c8c8eef8fb4cff66f
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-04-01 15:05:36 +0800

    Update flood_fill.c

--- level3/flood_fill.c
+++ level3/flood_fill.c
@@ -105,10 +105,10 @@ void fill(char **tab, t_point size, t_point cur, char to_fill)
 	//if the condition met then fill it with "F"
 	tab[cur.y][cur.x] = 'F';
 	//use recursion to check each position
-	fill(tab, size, (t_point){cur.y-1, cur.x}, to_fill);
-	fill(tab, size, (t_point){cur.y+1, cur.x}, to_fill);
-	fill(tab, size, (t_point){cur.y, cur.x-1}, to_fill);
-	fill(tab, size, (t_point){cur.y, cur.x+1}, to_fill);
+	fill(tab, size, (t_point){cur.x-1, cur.y}, to_fill);
+	fill(tab, size, (t_point){cur.x+1, cur.y}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y-1}, to_fill);
+	fill(tab, size, (t_point){cur.x, cur.y+1}, to_fill);
 }
 
 void  flood_fill(char **tab, t_point size, t_point begin)
