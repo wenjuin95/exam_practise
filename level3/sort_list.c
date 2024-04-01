3b44dae9d55ef3dc4ffc3a2cd35e365fc07c001f
Ryu <130266766+wenjuin95@users.noreply.github.com>
2024-04-01 20:14:00 +0800

    Update sort_list.c

--- level3/sort_list.c
+++ level3/sort_list.c
@@ -55,3 +55,48 @@ t_list	*sort_list(t_list *lst, int (*cmp)(int, int))
 	lst = tmp;
 	return (lst);
 }
+
+
+#include <stdio.h>
+#incldue <stdlib.h>
+
+int ascending(int a, int b)
+{
+	return (a <= b);
+}
+
+int	main(void)
+{
+	t_list *c = malloc(sizeof(t_list));
+	c->next = 0;
+	c->data = 45;
+
+	t_list *b = malloc(sizeof(t_list));
+	b->next = c;
+	b->data = 73;
+
+	t_list *a = malloc(sizeof(t_list));
+	a->next = b;
+	a->data = 108;
+
+	t_list *cur = a;
+	while (cur)
+	{
+		printf("%d", cur->data);
+		if (cur->next != 0)
+			printf(", ");
+		cur = cur->next;
+	}
+	printf("\n");
+
+	cur = sort_list(a, ascending);
+
+	while (cur)
+	{
+		printf("%d", cur->data);
+		if (cur->next != 0)
+			printf(", ");
+		cur = cur->next;
+	}
+	printf("\n");
+}
